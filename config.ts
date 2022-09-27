306955c395e3bcf66c9dd0a02a0530fc6f600fe8
BurtChan <105471886+BurtSweet@users.noreply.github.com>
2022-09-27 23:55:09 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,13 +1,13 @@
 export default {
-  title: "yun yu yuan", // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: "yunyuyuan", // github账户名
+  title: "BurtSweet", // 网站标题
+  nickName: "BurtSweet", // 昵称
+  githubName: "BurtSweet", // github账户名
   githubRepo: "nuxt3-blog", // 勿修改
   githubBranch: "master", // 勿修改
-  domain: "https://blog.halberd.cn", // rss域名
-  SEO_title: " - yunyuyuan blog",
-  SEO_description: "云与原的博客，书写值得书写的东西",
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客",
+  domain: "https://blog.yanjun.eu.org/", // rss域名
+  SEO_title: " - BurtSweet blog",
+  SEO_description: "苓椛杍的博客，书写值得书写的东西",
+  SEO_keywords: "BurtSweet's blog",
 
   themeColor: "#2aa0bb", // 主题色
   CommentRepoId: "R_kgDOGRGGfg", // https://giscus.app/zh-CN
